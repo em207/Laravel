6100ab0356814d2c5c40abff592a78857912281d
iamjustakid <m.bilal.younas1@live.com>
2019-01-20 23:04:38 +0500

    compose update

--- config/elasticblue.php
+++ config/elasticblue.php
@@ -3,6 +3,7 @@
 return [
     'connection' => [
         'hosts' => ['https://iysuhfpcx9:2jisc2zdbt@ash-127634581.eu-west-1.bonsaisearch.net'],
+        'port' => 443,
         'logger' => Elasticsearch\ClientBuilder::defaultLogger(storage_path('logs/elastic-search.log'), 400),
         'retries' => 1,
     ],
