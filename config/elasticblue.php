007d34ffdb657663db2593d04bdf2c7131485c31
iamjustakid <m.bilal.younas1@live.com>
2019-01-20 21:28:45 +0500

    Elasticsearch host updated

--- config/elasticblue.php
+++ config/elasticblue.php
@@ -2,7 +2,7 @@
 
 return [
     'connection' => [
-        'hosts' => ['https://iysuhfpcx9:2jisc2zdbt@ash-127634581.eu-west-1.bonsaisearch.net:9200'],
+        'hosts' => ['ash-127634581:9200'],
         'logger' => Elasticsearch\ClientBuilder::defaultLogger(storage_path('logs/elastic-search.log'), 400),
         'retries' => 1,
     ],
