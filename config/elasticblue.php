e1993be5fe09df4d92c7c5ac0a6b354932cdca9c
iamjustakid <m.bilal.younas1@live.com>
2019-01-20 18:43:09 +0500

    Composer Json Update

--- config/elasticblue.php
+++ config/elasticblue.php
@@ -2,10 +2,10 @@
 
 return [
     'connection' => [
-        'hosts' => ['localhost:9200'],
+        'hosts' => ['192.168.10.10:9200'],
         'logger' => Elasticsearch\ClientBuilder::defaultLogger(storage_path('logs/elastic-search.log'), 400),
         'retries' => 1,
     ],
 
-    'pagination' => 30,
+    'pagination' => 30
 ];
