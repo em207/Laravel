3d74807012b8b5777fffc65cd380699aa7e9d156
em207 <34325700+em207@users.noreply.github.com>
2019-01-21 17:22:18 +0100

    j ai commente une partie du code dans ressources/assets/js/components/Home.vue

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -26,36 +26,7 @@
             </div>
         </div>
         <!-- Modal -->
-        <!-- <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog">
-
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Aide</h4>
-                    </div>
-                    <div class="modal-body" style="font-weight:bold ">
-                        <p>2 types de recherches sont possibles :<br/></p>
-                        <ul>
-                            <li><u>Recherche exacte</u> en tapant le mot complet. Exemple : <b style="color: #3097D1;">chat</b>
-                            </li>
-                            <li><u>Recherche approximative</u> en préfixant le mot d'un <b
-                                    style="color: #3097D1;">.*</b>ou en
-                                le rajoutant à la fin
-                                <br/>Exemple : <b style="color: #3097D1;">.*chat</b> retournera achat et <b
-                                        style="color: #3097D1;">chat.*</b> retournera chaton
-                            </li>
-                        </ul>
-                        <p><b>Ps:</b> il est également possible d'utiliser des expressions régulières plus complèxes !
-                        </p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
+        
     </div>
 </template>
 <script>
